39c1e3851e17445307ff78b5319a04b4c0123e3a
Try <try@gmail.com>
2024-07-27 15:25:31 +0300

    User entity

---
+++ src/dating_backend/domain/entities/user.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Optional, Union
+
+from dating_backend.domain.value_objects.user_id import UserId
+
+
+@dataclass
+class User:
+    id: UserId
+    full_name: Optional[str]
+    username: Optional[str]
+    is_active: Optional[bool]
+
+    def __hash__(self) -> int:
+        return hash(self.user_id)
+
+    def __eq__(self, other: Union[object, "User"]) -> bool:
+        if not isinstance(other, User):
+            return False
+
+        return self.user_id == other.user_id
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__qualname__} object(id={self.user_id}, is_active"
+            f"={bool(self)})"
+        )
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__qualname__} <{self.user_id}>"
